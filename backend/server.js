3305a9c487c1aef6b173381436d38c9c123ffc16
Diego Rubi Salas <diegoalru@gmail.com>
2023-06-21 00:22:48 -0600

    Habilitación de cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,12 @@ import { Server as SocketServer } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new SocketServer(server);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:5173"
+  }
+});
+
 
 io.on("connection", (socket) => {
   console.log("A user connected");
