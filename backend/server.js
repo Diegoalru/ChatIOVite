eea42d6e18f4514486e19cb1c9d38b7451933650
Diego Rubi Salas <diegoalru@gmail.com>
2023-06-21 02:45:09 -0600

    Servidor HTTP a la escucha.

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: "http://localhost:5173"
-  }
+    origin: "http://localhost:5173",
+  },
 });
-
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
